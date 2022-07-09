dceab4eb43351bcfd28cac2de1449d99fbdbb49e
Jason Li <jason.li@playtravel.com.au>
2022-07-10 01:01:59 +1000

    added functions in api and request helper

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,8 @@
-export interface ApiService { }
+import { Song } from "../models/song";
+import { Collection } from "../models/collection";
+import { SuccessOrError } from "../helper/request-helpers";
+
+export interface ApiService {
+  searchSongs(searchValue: string): Promise<SuccessOrError<Array<Song>, string>>;
+  lookUpSongsInAlbum(collectionId: number): Promise<SuccessOrError<Collection, string>>;
+}
