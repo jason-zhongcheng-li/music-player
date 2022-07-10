6f95d26737384358462836241abbf3cfda10a7d3
Jason Li <jason.li@playtravel.com.au>
2022-07-10 11:26:20 +1000

    added classnames dependency and search bar component

---
+++ src/components/search/search-bar.tsx
@@ -0,0 +1,56 @@
+import { useState, useCallback } from 'react';
+import classnames from 'classnames';
+import { debounce } from 'lodash';
+import { Svg } from '../svg/svg';
+import { useITunes } from '../../contexts/MusicProvider';
+
+import styles from './search-bar.module.scss';
+
+export interface SearchBarProps {}
+
+const SearchBar = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(null);
+  const { searchSongs } = useITunes();
+
+  const [currentValue, setCurrentValue] = useState<string>();
+
+  const handleOnValueChange = useCallback(
+    debounce(async (value: string) => {
+      if (!value || value.trim().length === 0) {
+        setIsLoading(false);
+        return;
+      }
+      await searchSongs(value);
+      setIsLoading(false);
+    }, 250),
+    []
+  );
+
+  return (
+    <div className={styles.searchBar}>
+      {isLoading ? <i className={styles.loading} /> : <Svg name="search" className={styles.searchIcon} />}
+      <input
+        type="text"
+        value={currentValue || ''}
+        placeholder="Search for song"
+        className={classnames(styles.searchBarInput)}
+        onChange={(event) => {
+          const { value } = event.target;
+          setIsLoading(true);
+          setCurrentValue(value);
+          handleOnValueChange(value);
+        }}
+        onFocus={(event) => {
+          const { value } = event.target;
+          if (value) {
+            setCurrentValue(value);
+            handleOnValueChange(value);
+          }
+        }}
+        onClick={() => {}}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
