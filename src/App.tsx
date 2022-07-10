78622349b45be172ed70e03bea4d8b40abf3b670
Jason Li <jason.li@playtravel.com.au>
2022-07-10 11:14:52 +1000

    added app-provider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { MainLayout } from './layouts/main';
+import { AppProvider } from './components/app-provider/app-provider';
 import MediaPlayer from './components/media-player/media-player';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <MainLayout>
-        <MediaPlayer />
-      </MainLayout>
-    </div>
+    <AppProvider>
+      <div className="App">
+        <MainLayout>
+          <MediaPlayer />
+        </MainLayout>
+      </div>
+    </AppProvider>
   );
 }
 
