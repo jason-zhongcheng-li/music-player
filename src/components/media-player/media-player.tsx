3e845e318e3af5b24336b1092e06a845ce8b4078
Jason Li <jason.li@playtravel.com.au>
2022-07-10 11:47:19 +1000

    style the song list

--- src/components/media-player/media-player.tsx
+++ src/components/media-player/media-player.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import SearchBar from '../search/search-bar';
+import SongList from '../song-list/song-list';
 
 import styles from './media-player.module.scss';
 
 const MediaPlayer = () => {
   return (
     <div className={styles.mediaPlayer}>
-      <div className={styles.albumList}></div>
+      <div className={styles.albumList}>
+        <SearchBar />
+        <SongList />
+      </div>
       <div className={styles.songList}></div>
     </div>
   );
--- src/components/song-list/song-list.tsx
+++ src/components/song-list/song-list.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import { Image } from '../image/image';
 import { Song } from '../../models/song';
 import { useITunes } from '../../contexts/MusicProvider';
 
 import styles from './song-list.module.scss';
 
 const SongList = () => {
-  const { songs } = useITunes();
+  const { songs, lookupSongsInAlbum } = useITunes();
+  console.log('songs = ', songs);
+  const getSongsInAlbum = async (collectionId: number) => {
+    await lookupSongsInAlbum(collectionId);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -13,9 +17,17 @@ const SongList = () => {
         ?.filter((song: Song) => !!song.collectionId)
         .map((song: Song) => (
           <div className={styles.track} key={song.trackId}>
-            <div className={styles.trackName}>{song.trackCensoredName}</div>
-            <div className={styles.artistName}>{song.artistName}</div>
-            <div className={styles.collectionName}>{song.collectionCensoredName}</div>
+            <Image image={{ url: song.artworkUrl60 }} />
+            <div
+              className={styles.trackInfo}
+              role="button"
+              tabIndex={0}
+              onClick={() => getSongsInAlbum(song.collectionId)}
+            >
+              <div className={styles.trackName}>{song.trackCensoredName}</div>
+              <div className={styles.artistName}>{song.artistName}</div>
+              <div className={styles.collectionName}>{song.collectionCensoredName}</div>
+            </div>
           </div>
         ))}
     </div>
